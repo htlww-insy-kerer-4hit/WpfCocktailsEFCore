b3fa2e8339d43b057117b2a9f8328a3fd796fd40
Clemens Kerer <clemens.kerer@htlwienwest.at>
2022-03-09 07:22:13 +0100

    Initial commit for 'WPF cocktails with EF Core' template repository

---
+++ CocktailsWPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
+
+namespace CocktailsWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lbCocktails.ItemsSource = new string[] { "Cocktail 1", "Cocktail 2" };
+            lbCocktails.SelectedIndex = 0;
+            DataContext = this;
+        }
+
+        private void tbCocktailFilter_KeyUp(object sender, KeyEventArgs e)
+        {
+            string searchterm = tbCocktailFilter.Text.Trim().ToLower();
+            MessageBox.Show("Filtering is not implemented yet.");
+        }
+
+        private void btnEditCocktail_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Editing is not implemented yet.");
+        }
+    }
+}
